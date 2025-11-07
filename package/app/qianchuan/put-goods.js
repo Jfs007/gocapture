5553031e1b4c28a0e9e378b67ad048b7e3d0af27
shijiafeng <shijiafeng@tongmingmedia.com>
2025-11-07 11:05:52 +0800

    .

--- package/app/qianchuan/put-goods.js
+++ package/app/qianchuan/put-goods.js
@@ -3,12 +3,44 @@
     if (location.href.indexOf('https://qianchuan.jinritemai.com/creation/uni-prom-product') < 0) return;
     const initApp = () => {
 
+        const insetCostInput = () => {
+            if(document.querySelector('#costInputDiv')) return;
+            const costInputDiv = document.createElement('div');
+            costInputDiv.id = 'costInputDiv';
+
+            const target = document.querySelector('#overAllRoiBlock');
+            target.parentNode.insertBefore(costInputDiv, target.nextSibling);
+            costInputDiv.innerHTML = `<div class='oc-row'>
+                                            <div class='oc-title'>
+                                                <div class='oc-title-prefix'>
+                                                    <div class='oc-typography oc-typography-bold oc-typography-size-sm oc-typography-color-default oc-typography-type-paragraph oc-typography-span-undefined oc-title-text'>保本成本</div>
+                                                </div>
+                                            </div>
+                                            <div class='oc-space oc-space-vertical'>
+                                                <input style="background-color: #f4f4f5;height: 32px;padding: 4px 12px;border-radius: 3px;border: none;outline: none;" />
+                                            </div>
+                                        </div>`;
+            costInputDiv.addEventListener('input', (e) => {
+                const value = e.target.value;
+                console.log('保本成本变化了', value);
+            })
+
+
+        }
+
 
         const api_hook = {
-            'ad/api/creation/v1/audit/async-check-product|repeat': function (data, fullData) {
+            'ad/api/creation/v1/audit/async-check-product|repeat': function (data) {
                 console.log(data, fullData, '数据来了');
+                const { url } = data;
+                const [_, productid] = url.match(/.*product=([\d,]+)/) || [];
+                productid && insetCostInput();
+
             }
         }
+
+
+
         function listenMessage(event) {
             const { type, data } = event.data;
             if (type === "WEB_REQUEST_RESPONSE") {
