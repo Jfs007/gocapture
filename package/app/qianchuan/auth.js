005b63d5b1bb06c5dc7ba4b97d94383dcd20cece
shijiafeng <shijiafeng@tongmingmedia.com>
2025-11-25 17:42:51 +0800

    .

--- package/app/qianchuan/auth.js
+++ package/app/qianchuan/auth.js
@@ -3,10 +3,11 @@
     if (location.href.indexOf('https://business.oceanengine.com/site/index') < 0) return;
     const mdChrome = _require("mdChrome");
     setTimeout(() => {
+        console.log('mdChrome,');
         // if (location.href === 'https://business.oceanengine.com/brand/index' || location.href.indexOf('https://business.oceanengine.com/site/index') > -1) {
         mdChrome.web.cmd({
             cmd: 'openPopup'
-        })
+        });
         // }
         // auth.doAuth({ host: 'business.oceanengine.com', value: getUserInfo() });
 
