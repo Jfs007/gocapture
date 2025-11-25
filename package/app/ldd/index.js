a422257d1e74d03efb9f391f9f36bd9766930775
shijiafeng <shijiafeng@tongmingmedia.com>
2025-11-25 16:26:33 +0800

    .

--- package/app/ldd/index.js
+++ package/app/ldd/index.js
@@ -1,5 +1,5 @@
 !function () {
-    if (location.href.indexOf('localhost') < 0 || location.href.indexOf('.itaored.com') < 0) return;
+    if (!(location.href.indexOf('localhost') > -1 || location.href.indexOf('.itaored.com') > -1)) return;
     const token = localStorage.getItem('TOKEN');
     console.log(token, 'ldd-token');
 }()
