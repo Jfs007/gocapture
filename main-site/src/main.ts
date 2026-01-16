02d9bcf8bc82571675e76b0ed3c8271708fc23ee
shijiafeng <shijiafeng@tongmingmedia.com>
2026-01-16 17:32:09 +0800

    .

--- main-site/src/main.ts
+++ main-site/src/main.ts
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style/index.css'
 let app: any = null;
-
 window.addEventListener('message', (e) => {
     
     if (e.data.cmd == 'install-setup' && !app) {
         const env = e.data.env || {};
         window.__PLG__ENV__ = env;
         app = createApp(App)
-        app.mount('#app')
+        app.mount('#app');
+        window.__HIDE_GLOBAL_LOADER__ && window.__HIDE_GLOBAL_LOADER__();
     }
 })
 
