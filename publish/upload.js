89a7f125c790657fb574566f8e8a8321bbc196c1
shijiafeng <shijiafeng@tongmingmedia.com>
2026-01-26 16:59:43 +0800

    一键代发/支持抖音面单

--- publish/upload.js
+++ publish/upload.js
@@ -11,7 +11,7 @@ const execAsync = promisify(exec);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const accessToken = '0021bb2eb16a7e2dbfd32760c9d49c5f';
+const accessToken = 'edd630999e1f66721e318241e64f323f';
 // 配置
 const config = {
   accessKeyId: '',
