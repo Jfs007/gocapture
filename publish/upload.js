a15a8f04b5fc7bd663ec3bb44316511090408f67
shijiafeng <shijiafeng@tongmingmedia.com>
2026-03-31 17:11:29 +0800

    .

--- publish/upload.js
+++ publish/upload.js
@@ -11,7 +11,7 @@ const execAsync = promisify(exec);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const accessToken = 'd2d25c5a252728cf0c0383e1ab2dc7e6';
+const accessToken = '';
 // 配置
 const config = {
   accessKeyId: '',
