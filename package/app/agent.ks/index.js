400b6bb83aa9c5b36e1531997bbed35c9e4db396
shijiafeng <shijiafeng@tongmingmedia.com>
2026-04-08 17:57:55 +0800

    .

--- package/app/agent.ks/index.js
+++ package/app/agent.ks/index.js
@@ -1,20 +1,42 @@
 !function () {
+    const href = location.href;
     // const mdChrome = _require('mdChrome');
-    console.log("HELLO AGENT>KS");
     const search = new URLSearchParams(window.location.search);
-    const AUTH = search.get('LDD_NIU_AUTH');
-    console.log(AUTH, 'auth ks');
-    if (AUTH != 1) return;
+    // console.log('__WEB_REQUEST_API__', window.__WEB_REQUEST_API__);
+    __WEB_REQUEST_API__.onResponse(({ url, result, request, method, modified }) => {
+        const AUTH_REDIREURL = search.get('AUTH_REDIREURL');
+        const redirectUrlSearch = ((AUTH_REDIREURL || '').replaceAll('@', '&').replace('&', '?'));
+        const [, agentId] = redirectUrlSearch.match(/agentId=(\d*)/) || []
+        if (url.indexOf('dsp/agent/extra/info') > -1) {
+            
+            const data = result?.data;
+            const agent = data.find(agent => agent.agentId == agentId) || {};
+            if (href.indexOf('jinfu.e') > -1) {
+                const AUTH_REDIREURL = search.get('AUTH_REDIREURL');
+                if (!AUTH_REDIREURL) return;
+                const rhref = `https://niu.e.kuaishou.com/` + (redirectUrlSearch.replace('AGENTUSERID', agent.agentUserId || ''));
+                console.log('AUTH_REDIREURL', rhref);
+                window.location.href = rhref;
+            }
+            // console.log('dsp/agent/extra/info', result);
+        }
+    });
+    
 
-    const xpath = `//*[@id="root"]/section/section/main/div/div[1]/div[2]/div[2]`;
-    const btn = document.evaluate(
-        xpath,
-        document,
-        null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE,
-        null
-    ).singleNodeValue
-    btn?.click()
 
+    if (href.indexOf('niu.e') > -1) {
+        const AUTH = search.get('LDD_NIU_AUTH');
+        if (AUTH != 1) return;
+
+        const xpath = `//*[@id="root"]/section/section/main/div/div[1]/div[2]/div[2]`;
+        const btn = document.evaluate(
+            xpath,
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null
+        ).singleNodeValue
+        btn?.click()
+    }
 
 }()
