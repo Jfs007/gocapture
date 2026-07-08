d1741455e4d134786c00fbb3542fd8a35135f89a
shijiafeng <shijiafeng@tongmingmedia.com>
2026-07-08 16:37:31 +0800

    前置检索优化

--- package/app/magnus/index.js
+++ package/app/magnus/index.js
@@ -9981,7 +9981,7 @@ ${hit.preciseSnippet || hit.uniqueSnippet}`);
     __name: "ComposerPanel",
     setup(__props, { expose: __expose }) {
       const composerInputRef = /* @__PURE__ */ ref(null);
-      const buildVersion = "20260707.235056.379";
+      const buildVersion = "20260708.163343.942";
       const commands = useMagnusCommands();
       const appUiStore = useAppUiStore();
       const composerStore = useComposerStore();
